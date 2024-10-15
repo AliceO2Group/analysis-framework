7e0e4e18ec8b26dea08cd453cf6e942e4393adb5
Vít Kučera <vit.kucera@cern.ch>
2024-10-15 17:40:56 +0200

    Add system info and ALIBUILD_WORK_DIR in the setup script

--- docs/troubleshooting/summarise_o2p_setup.sh
+++ docs/troubleshooting/summarise_o2p_setup.sh
@@ -2,6 +2,24 @@
 
 # Summarise O2Physics setup
 
+# System info
+
+systemName=""
+user=""
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  systemName="$(lsb_release -ds)"
+  user="$(whoami)"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  systemName="$(sw_vers -productName) $(sw_vers -productVersion)"
+  user="$(whoami)"
+else
+  echo "Unrecognised OS type: \"${OSTYPE}\""
+fi
+echo "OS: ${systemName}"
+echo "User: ${user}"
+
+# aliBuild info
+
 if [[ -n "$(which aliBuild)" ]]; then
   echo "aliBuild location: $(which aliBuild)"
   # echo "all aliBuild locations: $(whereis aliBuild)"
@@ -10,10 +28,20 @@ else
   echo "aliBuild not found."
 fi
 
+if [[ -n "$ALIBUILD_WORK_DIR" ]]; then
+  echo "ALIBUILD_WORK_DIR=\"${ALIBUILD_WORK_DIR}\""
+else
+  echo "ALIBUILD_WORK_DIR is not set."
+fi
+
+# Repository info
+
 for repo in alidist O2 O2Physics; do
   [[ -d "${repo}" ]] || { echo "Directory ${repo} not found in the current directory."; continue; }
   echo "Last commit of ${repo}: $(cd "${repo}" && git log -n 1 --pretty="format:%ci %h")"
 done
+
+# Package build info
 
 cmdStat=""
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
@@ -22,7 +50,7 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
   cmdStat="stat -f %Sm"
 fi
 
-if [[ -n "$cmdStat" ]]; then
+if [[ -n "$cmdStat" && -n "$ALIBUILD_WORK_DIR" ]]; then
   for pkg in O2 O2Physics; do
     log="$ALIBUILD_WORK_DIR/BUILD/${pkg}-latest/log"
     [[ -f "${log}" ]] || { echo "Log file ${log} not found."; continue; }
